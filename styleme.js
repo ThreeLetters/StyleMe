3608732607d9e6a5e2198d990d99e94488951b36
Andrew54757 <andrews54757@gmail.com>
2016-08-23 16:09:37 -0400

    Update styleme.js

--- styleme.js
+++ styleme.js
@@ -1,6 +1,8 @@
-module.exports = function() {
-
-
+var avail = JSON.parse(fs.readFileSync('./styles',"utf8"))
+module.exports = function(str,style) {
+  if (!style) throw "Style not specified";
+  var styles = style.split("|")
+  
 
 
 
