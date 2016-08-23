63dceb4a9417fe4b486d841e1ef1fd2adb52f82c
Andrew54757 <andrews54757@gmail.com>
2016-08-23 16:11:00 -0400

    Update styleme.js

--- styleme.js
+++ styleme.js
@@ -1,10 +1,15 @@
-var avail = JSON.parse(fs.readFileSync('./styles',"utf8"))
+var avail = JSON.parse(fs.readFileSync('./styles.json',"utf8"))
 module.exports = function(str,style) {
   if (!style) throw "Style not specified";
   var styles = style.split("|")
-  
+  var final = "";
+  styles.forEach((sty)=>{
+    final += avail[sty]
+    
+  })
+  final += str
 
-
+return str
 
 
 }
