3971d4e28c3302359e77951d5e39378c2a4149e8
Andrew54757 <andrews54757@gmail.com>
2016-09-18 16:44:59 -0400

    Update stringProto.js

--- stringProto.js
+++ stringProto.js
@@ -1,7 +1,11 @@
 
 module.exports = function() {
   String.prototype.styleMe = function() {
-    
+    for (var i = 0; i < this.length; i ++) {
+      var a = this.indexOf()
+      
+      
+    }
   }
   
   
