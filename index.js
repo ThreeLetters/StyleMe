94fc87e9e08a99c955fe641630c1aac8920e12c5
Andrew54757 <andrews54757@gmail.com>
2016-09-20 14:38:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var styleme = require('./styleme')
 var extend = require('./stringProto')
 var send = {};
+  var list = JSON.parse(require('fs').readFileSync(__dirname + '/styles.json',"utf8"))
+  var colors = {};
+  for (var i in list)
+  {
+    
+    eval("colors." + i + " = \"" + list[i].replace(/\|/g,"\\") + "\"")
+    
+  }
 var functions = {
   style: styleme,
   extend: function() {
-    return extend(send)
+    return extend(send,colors)
     
   },
 }
 var special = require('./special/')
-var colors = JSON.parse(require('fs').readFileSync(__dirname + "/styles.json"))
 for (var i in special) {
   var a = special[i].toString();
   var b = a.indexOf("(") + 1
@@ -20,7 +27,7 @@ for (var i in special) {
   if (c) {
   for (var k in colors) {
       if (a.indexOf(c + "." + k) == -1) continue;
-a = a.replace(new RegExp(c + "\\." + k,"g"),"\"" + colors[k].replace(/\|/g,"\\") + "\"");
+a = a.replace(new RegExp(c + "\\." + k,"g"),"\"" + colors[k] + "\"");
   
   } 
   }
@@ -28,11 +35,11 @@ a = a.replace(new RegExp(c + "\\." + k,"g"),"\"" + colors[k].replace(/\|/g,"\\")
   eval("functions."+ i + "=" + g)
   send[i] = g
 }
-var b = colors
-for (var i in b) {
-  var h = b[i];
+
+for (var i in colors) {
+  var h = colors[i];
     if (!h) continue;
-  eval("functions." + i + "=function(a){return \"" + h.replace(/\|/g,"\\") +"\" + a;}");
+  eval("functions." + i + "=function(a){return \"" + h +"\" + a;}");
   
 }
 
