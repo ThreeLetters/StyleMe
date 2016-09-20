c72b3efef5eb5e90a4af15465d87c4145e2953fb
Andrew54757 <andrews54757@gmail.com>
2016-09-20 14:48:39 -0400

    Update random.js

--- special/random.js
+++ special/random.js
@@ -1,5 +1,13 @@
 module.exports = function(a,i,colors) {
-
+var color = {
+  red: colors.red,
+  gre: colors.gre,
+  blu: colors.blu,
+  yel: colors.yel,
+  mag: colors.mag,
+  whi: colors.whi,
+  cya: colors.cya
+}
 
 
 }
