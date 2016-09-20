d2ac31ea9e533b3e93598e9df1ac7b13cbe6b581
Andrew54757 <andrews54757@gmail.com>
2016-09-20 11:02:03 -0400

    Update rainbow.js

--- special/rainbow.js
+++ special/rainbow.js
@@ -1,5 +1,21 @@
 module.exports = function(l,i) {
-  
+  switch (i%5) {
+    case 0:
+    return "" + l
+    break;
+    case 1:
+      return "" + l
+    break;
+    case 2:
+      return "" + l
+    break;
+    case 3:
+      return "" + l
+    break;
+    case 4:
+      return "" + l
+    break;
+  }
   
   
 }
