e672a18bf50630b1722c028fa6c4e95976bc5f48
Andrew54757 <andrews54757@gmail.com>
2016-09-20 14:02:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ var a = {
   extend: extend,
 }
 var b = JSON.parse(require('fs').readFileSync(__dirname + "/styles.json"))
+for (var i in b) {
+  var h = b[i];
+    if (!h) continue;
+  eval("a." + i + "=function(){return \"" + h.replace(/\|/g,"\\") +"\" + this;}");
+  
+}
 
