ec8b88a6bd11137855a084eba26739f1c40eda0b
Andrew54757 <andrews54757@gmail.com>
2016-09-20 14:01:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 var styleme = require('./styleme')
 var extend = require('./stringProto')
-module.exports = {
+var a = {
   style: styleme,
   extend: extend,
-  res: function(a) {return "\x1b[0m" + a},
-  bri: function(a) {return "\x1b[1m" + a},
-  dim: function(a) {return "\x1b[2m" + a},
-  und: function(a) {return "\x1b[4m" + a},
-  bli: function(a) {return "\x1b[5m" + a},
-  rev: function(a) {return "\x1b[7m" + a},
-  hid: function(a) {return "\x1b[8m" + a},
-  bla: function(a) {return "\x1b[30m" + a},
-  red: function(a) {return "\x1b[31m" + a},
-  gre: function(a) {return "\x1b[32m" + a},
-  yel: function(a) {return "\x1b[33m" + a},
-  blu: function(a) {return "\x1b[34m" + a},
-  mag: function(a) {return "\x1b[35m" + a},
-  cya: function(a) {return "\x1b[36m" + a},
-  whi: function(a) {return "\x1b[37m" + a},
-  bbl: function(a) {return "\x1b[44m" + a},
-  bre: function(a) {return "\x1b[41m" + a},
-  bgr: function(a) {return "\x1b[42m" + a},
-  bye: function(a) {return "\x1b[43m" + a},
-  bma: function(a) {return "\x1b[45m" + a},
-  bcy: function(a) {return "\x1b[46m" + a},
-  bwh: function(a) {return "\x1b[47m" + a},
-  end: function(a) {return a + "\x1b[0m"}
 }
+var b = JSON.parse(require('fs').readFileSync(__dirname + "/styles.json"))
+
