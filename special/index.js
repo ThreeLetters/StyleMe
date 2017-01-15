c00c89f96d1579c62c7a3696415e108e689d9d72
Andrew S <andrews54757@gmail.com>
2017-01-15 13:59:30 -0500

    Update index.js

--- special/index.js
+++ special/index.js
@@ -2,5 +2,6 @@ module.exports = {
   ame: require('./america.js'),
   rai: require('./rainbow.js'),
   ran: require('./random.js'),
-  zeb: require('./zebra.js')
+  zeb: require('./zebra.js'),
+  bnw: require('./blacknwhite.js')
 }
